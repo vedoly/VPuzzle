cd607ca7cf57e3ccd0386d3be64e918ebbb8d308
zaltzea <ravipas.apk@gmail.com>
2020-01-23 14:52:50 +0700

    Fix Webhook Posting

--- project/__init__.py
+++ project/__init__.py
@@ -11,7 +11,7 @@ ans_state = {"userid":"ans"}
 index = 1
 Q=['',
 'https://res.cloudinary.com/velody750/image/upload/v1579667082/samples/VPuzzle/1_vsxw5v.jpg',
-'https://res.cloudinary.com/velody750/image/upload/v1579667082/samples/VPuzzle/2_pof9i8.jpg',
+'https://res.cloudinary.com/velody750/image/upload/v1579760259/samples/VPuzzle/83133340_468907533984747_5003225175685070848_n_yrlux0.jpg',
 'https://res.cloudinary.com/velody750/image/upload/v1579667083/samples/VPuzzle/3_judc9j.jpg',
 'https://res.cloudinary.com/velody750/image/upload/v1579667083/samples/VPuzzle/4_gdv5gu.jpg',
 'https://res.cloudinary.com/velody750/image/upload/v1579667083/samples/VPuzzle/5_takj1a.jpg',
@@ -19,8 +19,8 @@ Q=['',
 'https://res.cloudinary.com/velody750/image/upload/v1579667083/samples/VPuzzle/7_h17ooq.jpg',
 'https://res.cloudinary.com/velody750/image/upload/v1579667082/samples/VPuzzle/8_atlhyf.jpg',
 'https://res.cloudinary.com/velody750/image/upload/v1579667082/samples/VPuzzle/9_r3gm2q.jpg',
-'https://res.cloudinary.com/velody750/image/upload/v1579667083/samples/VPuzzle/10_tkilrn.jpg']
-ANS=['',"ตาราง","รองเท้า","คนสำคัญ","ฝรั่งเศส","อินทริเกต","กลศาสตร์","สยามพารากอน","เนตรนารี","ดัชนีหักเห","ส่วนเบี่ยงเบนมาตรฐาน"]
+'https://res.cloudinary.com/vel ody750/image/upload/v1579667083/samples/VPuzzle/10_tkilrn.jpg']
+ANS=['',"ตาราง","skooldio","คนสำคัญ","ฝรั่งเศส","อินทริเกต","กลศาสตร์","สยามพารากอน","เนตรนารี","ดัชนีหักเห","ส่วนเบี่ยงเบนมาตรฐาน"]
 
 #Q1 = ["https://drive.google.com/file/d/1qINPCg7qcIL0tBezJP_3L73TuLroSNI7/view?usp=sharing"]
 #Q1_follow = ["","","เท่าไหร่","มีเท่าไหร่","ทั้งหมดเท่าไหร่","มีทั้งหมดเท่าไหร่"]
@@ -36,41 +36,46 @@ def GET_BTC_PRICE():
 @app.route('/webhook', methods=['POST','GET'])
 def webhook():
     if request.method == 'POST':
-        payload = request.json
-
-        Reply_token = payload['events'][0]['replyToken']
-        print(payload)
-        user = payload["events"][0]['source']['userId']
-        print("vvvvvvvvvvvvvvvvvvvvvvvvvvvvv")
-        print(Reply_token)
-        message = payload['events'][0]['message']['text']
-        print(message)
-        if(user not in chat_state):
-            chat_state[user]=0
-            ans_state[user]=1
-        if chat_state[user] == 0:
-            ReplyImage(Reply_token,1,Channel_access_token,user)
-            chat_state[user] = 1
-       
-        elif chat_state[user] == 1:
-            if message == ANS[ans_state[user]] :
-                Reply_messasge = "ถูกต้องงงง พร้อมสำหรับข้อต่อไปหรือยัง (press any key to the next question)"
-                ReplyMessage(Reply_token,Reply_messasge,Channel_access_token)
-                chat_state[user] = 2
-            
-                
-            
-            elif message != ans_state[user] :
-                Reply_messasge = "ผิดจ้า ลองตอบใหม่นะ"
-                ReplyMessage(Reply_token,Reply_messasge,Channel_access_token)
+        print('Tyring post')
+        try:
+            payload = request.json
+            Reply_token = payload['events'][0]['replyToken']
+            print(payload)
+            print("ccccccAAAAAAAAAAAAAAAA")
+            user = payload["events"][0]['source']['userId']
+            print("vvvvvvvvvvvvvvvvvvvvvvvvvvvvv")
+            print(Reply_token)
+            message = payload['events'][0]['message']['text']
+            print(message)
+            if(user not in chat_state):
+                chat_state[user]=0
+                ans_state[user]=1
+            if chat_state[user] == 0:
+                ReplyImage(Reply_token,1,Channel_access_token,user)
+                chat_state[user] = 1
         
-        elif chat_state[user] == 2:
-            ans_state[user]+=1
-            ReplyImage(Reply_token,ans_state[user],Channel_access_token,user)
-            chat_state[user] = 1
+            elif chat_state[user] == 1:
+                if message == ANS[ans_state[user]] :
+                    Reply_messasge = "ถูกต้องงงง พร้อมสำหรับข้อต่อไปหรือยัง (press any key to the next question)"
+                    ReplyMessage(Reply_token,Reply_messasge,Channel_access_token)
+                    chat_state[user] = 2
+                
+                    
+                
+                elif message != ans_state[user] :
+                    Reply_messasge = "ผิดจ้า ลองตอบใหม่นะ"
+                    ReplyMessage(Reply_token,Reply_messasge,Channel_access_token)
+            
+            elif chat_state[user] == 2:
+                ans_state[user]+=1
+                ReplyImage(Reply_token,ans_state[user],Channel_access_token,user)
+                chat_state[user] = 1
 
 
-        return request.json, 200
+            return app.response_class(request.json, content_type='application/json')
+        except:
+            print('except')
+            return request.json, 200
 
     elif request.method == 'GET' :
         return 'this is method GET!!!' , 200
